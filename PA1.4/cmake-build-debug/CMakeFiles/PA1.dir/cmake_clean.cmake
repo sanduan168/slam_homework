d067fdaa1a31770a38a19b964147721d2c927887
sanduan168 <minzhao010@gmail.com>
2018-03-04 11:16:39 +0800

    add PA1.4

---
+++ PA1.4/cmake-build-debug/CMakeFiles/PA1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PA1.dir/src/hello.cpp.o"
+  "../lib/libPA1.pdb"
+  "../lib/libPA1.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PA1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ PA1.4/cmake-build-debug/CMakeFiles/useHello.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/useHello.dir/useHello.c.o"
+  "useHello.pdb"
+  "useHello"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/useHello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
